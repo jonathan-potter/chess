45dc603b6c5ae3279a4dcbe41e272e74478a5950
Sterling Forest / Jonathan Potter <sterling.kxz@gmail.com / jonathan.potter.business@gmail.com>
2013-08-27 10:56:39 -0400

    chess initialize and play method

--- chess/chess.rb
+++ chess/chess.rb
@@ -1,5 +1,20 @@
+require './board'
+require './players'
+
 class Chess
   attr_accessor :board, :players, :dead_pieces
 
+  def initialize
+    self.board = Board.new
+    self.players = [Player.new(:white), Player.new(:black)]
+  end
+
+  def play
+    until end_game?
+      player = self.players.pop
+      player.turn
+      self.players.shift(player)
+    end
+  end
 
 end
