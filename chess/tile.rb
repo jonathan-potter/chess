91380a0cd1474a0ad8980a88e2e74a665bc467c8
App Academy Student <student@appacademy.io>
2013-08-28 15:33:17 -0400

    separates each piece to its own file

--- chess/tile.rb
+++ chess/tile.rb
@@ -1,5 +1,11 @@
 # encoding: UTF-8
-require './piece'
+require './pieces/piece'
+require './pieces/pawn'
+require './pieces/rook'
+require './pieces/knight'
+require './pieces/bishop'
+require './pieces/queen'
+require './pieces/king'
 
 class Tile
   attr_accessor :piece, :position
